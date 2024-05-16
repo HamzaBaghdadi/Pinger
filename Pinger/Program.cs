78d5e9f63e3ab73539e5449fe49a388735e24576
Hamza Baghdadi <155057894+HamzaBaghdadi@users.noreply.github.com>
2024-05-16 11:45:19 +0300

    Create project

---
+++ Pinger/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Net.NetworkInformation;
+
+Ping ping = new Ping();
+PingReply pingReply;
+
+while (true)
+{
+    Console.WriteLine("Type IP:");
+    try
+    {
+        pingReply = ping.Send(Console.ReadLine()!);
+
+        if (pingReply.Status == IPStatus.Success)
+        {
+            Console.WriteLine($"Ping success: {pingReply.RoundtripTime} ms");
+        }
+        else
+        {
+            Console.WriteLine($"Ping failed: {pingReply.Status}");
+        }
+    }
+    catch (Exception ex) { Console.WriteLine("Wrong IP"); }
+    System.Threading.Thread.Sleep(1000);
+}

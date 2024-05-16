c3594ab85460e9886e3add3304a2ba3a5132c32f
Hamza Baghdadi <155057894+HamzaBaghdadi@users.noreply.github.com>
2024-05-16 12:12:45 +0300

    Creating project

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Pinger
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Pinger
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
